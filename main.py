1fbb3cb42eeb690c54ea6bcada71106596093596
Enrico <enrico.ragni.2004@gmail.com>
2020-03-23 10:40:40 +0100

    creati nuovi file

---
+++ main.py
@@ -0,0 +1,5 @@
+a=int(input("inserisci un numero: "))
+print("questo è il doppio", a*2)
+print("questo è il triplo: ", a*3)
+b=input("ora che hai visto i suoi primi 2 multipli è ora di farmi sapere il tuo nome: ")
+print(b, "è un bellissimo nome:)")
