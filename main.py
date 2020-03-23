becbe6b02468ad7361d04a9e00f098793863e8d4
Enrico <enrico.ragni.2004@gmail.com>
2020-03-23 10:47:11 +0100

    modificato main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print("questo è il doppio", a*2)
 print("questo è il triplo: ", a*3)
 b=input("ora che hai visto i suoi primi 2 multipli è ora di farmi sapere il tuo nome: ")
 print(b, "è un bellissimo nome:)")
+c=int(input("inserici un altro numero: "))
+for i in range(c):
+    i=c
+    print(i)
+    i+=1
